1b520a2b4daac0bf8d41df18239cce3412d0c576
dim-anis <anis.dim@gmail.com>
2025-11-15 15:44:04 +0400

    add onWholeMessage
    waits for a whole single message to be received before calling a
    callback

--- src/download.ts
+++ src/download.ts
@@ -14,8 +14,29 @@ function download(peer: Peer) {
   socket.connect(peer.port, peer.ip, () => {
     // socket.write()
   });
+  onWholeMessage(socket, (data) => {
+    // handle data
+  });
+}
 
-  socket.on("data", () => {
+function onWholeMessage(
+  socket: net.Socket,
+  cb: (data: Buffer<ArrayBuffer>) => void,
+) {
+  let savedBuf = Buffer.alloc(4);
+  let handshake = true;
+
+  socket.on("data", (recvBuf: Buffer<ArrayBufferLike>) => {
     // handle response
+    let msgLen = () =>
+      handshake ? savedBuf.readUint8(0) + 49 : savedBuf.readUint32BE(0) + 4;
+    // write to buf
+    savedBuf = Buffer.concat([savedBuf, recvBuf]);
+
+    while (savedBuf.length >= 4 && savedBuf.length >= msgLen()) {
+      cb(savedBuf.subarray(0, msgLen()));
+      savedBuf = savedBuf.subarray(msgLen());
+      handshake = false;
+    }
   });
 }
