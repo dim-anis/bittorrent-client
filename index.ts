6cb8e3c4147e7f723ed44a8fe0bd87d044fea4dc
dim-anis <anis.dim@gmail.com>
2025-12-10 15:38:24 +0400

    add error handling for torrent file path arg

--- index.ts
+++ index.ts
@@ -1,6 +1,13 @@
 import * as torrentParser from "./src/torrent-parser.ts";
 import download from "./src/download.ts";
 
+const torrentPath = process.argv[2];
+
+if (!torrentPath) {
+  console.log("argument <torrent-path> is required");
+  process.exit(1);
+}
+
 const torrent = torrentParser.open(process.argv[2]);
 
 async function main() {
