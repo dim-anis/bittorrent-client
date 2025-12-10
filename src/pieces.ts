ff98f868e5a05e5b5b7065b56ed70cb30e071111
dim-anis <anis.dim@gmail.com>
2025-12-10 15:50:38 +0400

    add hash validation for pieces
    - write pieces to buffer
    - verify piece hash
    - if hash invalid clear buffer

--- src/pieces.ts
+++ src/pieces.ts
@@ -1,3 +1,4 @@
+import crypto from "node:crypto";
 import { type Payload } from "./message.ts";
 import { showProgressBar } from "./progressBar.ts";
 import { BLOCK_LEN, blocksPerPiece } from "./torrent-parser.ts";
@@ -13,23 +14,28 @@ class Piece {
   requested: boolean;
   finished: boolean;
   blocks: BlockState[];
+  buffer: Buffer<ArrayBuffer>;
 
-  constructor(blocks?: BlockState[]) {
+  constructor(blocks: BlockState[], pieceLength: number) {
     this.requested = false;
     this.finished = false;
     this.blocks = blocks ? blocks : [];
+    this.buffer = Buffer.alloc(pieceLength);
   }
 }
 
 export class PieceManager {
   #pieces: Piece[];
+  #torrent: any;
 
   constructor(torrent: any) {
+    this.#torrent = torrent;
     this.#pieces = [];
     const nPieces = torrent.info.pieces.length / 20;
+    const pieceLength = torrent.info["piece length"];
     for (let i = 0; i < nPieces; i++) {
       this.#pieces.push(
-        new Piece(new Array(blocksPerPiece(torrent, i)).fill(0)),
+        new Piece(new Array(blocksPerPiece(torrent, i)).fill(0), pieceLength),
       );
     }
   }
@@ -42,12 +48,22 @@ export class PieceManager {
   markBlockFinished(pieceBlock: Payload): void {
     const blockIndex = pieceBlock.begin / BLOCK_LEN;
     this.#pieces[pieceBlock.index].blocks[blockIndex] = BlockState.finished;
+    const blockLen = pieceBlock.block?.length;
+
+    const pieceBuffer = this.#pieces[pieceBlock.index].buffer;
+    pieceBlock.block!.copy(pieceBuffer, pieceBlock.begin, 0, blockLen);
 
     if (this.isPieceComplete(pieceBlock.index)) {
+      if (!this.isHashValid(pieceBlock.index)) {
+        pieceBuffer.fill(0);
+        return;
+      }
+
       this.#pieces[pieceBlock.index].finished = true;
       this.logProgress();
     }
   }
+
   isBlockComplete(pieceBlock: Payload) {
     const blockIndex = pieceBlock.begin / BLOCK_LEN;
     return (
@@ -63,5 +79,33 @@ export class PieceManager {
     return this.#pieces[pieceIndex].blocks.every(
       (block) => block === BlockState.finished,
     );
+  }
+
+  isHashValid(pieceIndex: number): boolean {
+    const hash = crypto.createHash("sha1");
+    const pieceBuffer = this.#pieces[pieceIndex].buffer;
+    hash.update(pieceBuffer);
+    const targetHash = this.getPieceHash(pieceIndex);
+    const calculatedHash = hash.digest();
+
+    if (calculatedHash.equals(targetHash)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  logProgress() {
+    const totalPieces = this.#pieces.length;
+    const donwloadedPieces = this.#pieces.filter((p) => p.finished).length;
+
+    showProgressBar(totalPieces, donwloadedPieces);
+  }
+
+  getPieceHash(pieceIndex: number) {
+    const hashLength = 20;
+    const startIndex = pieceIndex * hashLength;
+    const endIndex = startIndex + hashLength;
+    return this.#torrent.info.pieces.subarray(startIndex, endIndex);
   }
 }
